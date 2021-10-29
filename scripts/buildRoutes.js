48190b3d093725911c8f86ff62d6df8be11c01d9
sanjayheaven <34485325+sanjayheaven@users.noreply.github.com>
2021-10-30 02:07:18 +0800

    1030|增加生成路由，开发隐藏路由

---
+++ scripts/buildRoutes.js
@@ -0,0 +1,40 @@
+const fs = require("fs")
+const path = require("path")
+const pluralize = require("pluralize") // 单词复数
+
+const createRoute = (app, file, controller) => {
+  let keys = Object.keys(controller)
+  let routes = keys.map((key) => {
+    let method = (key.startsWith("get") && "get") || "post"
+    return `router.${method}('/${key}',Controllers.${key})`
+  })
+  return `
+    const Router = require("koa-router")
+    const { routerPrefix } = require("../../config/config.router.js")
+    const router = new Router({prefix:routerPrefix+"/${pluralize(file)}" }) 
+    const Controllers = require("../../controllers/${app}/${file}")
+    ${routes.join("\n")}
+    module.exports = router
+    `
+}
+
+const main = () => {
+  let controllerPath = path.resolve(__dirname, "../controllers")
+  let apps = fs.readdirSync(controllerPath)
+  console.log(apps, 11111)
+  apps.forEach((app) => {
+    let appPath = path.resolve(__dirname, `../controllers/${app}`)
+    let files = fs.readdirSync(appPath)
+    console.log(files, 12222)
+    files.forEach((file) => {
+      let filePath = path.resolve(__dirname, `../controllers/${app}/`, file)
+      let controller = require(filePath)
+      let basicPath = path.resolve(__dirname, `../routes/${app}/`, file)
+      fs.mkdir(path.resolve(__dirname, `../routes/${app}`), () => {})
+      fs.writeFileSync(basicPath, createRoute(app, file, controller))
+      file = file.substring(0, file.indexOf("."))
+    }, {})
+  })
+}
+
+main()
