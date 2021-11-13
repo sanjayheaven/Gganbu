0df954d99170af2e6a6420e999c19e7744089bf8
sanjayheaven <34485325+sanjayheaven@users.noreply.github.com>
2021-11-14 05:34:55 +0800

    1114|修订als中间件错误

--- server.ts
+++ server.ts
@@ -2,21 +2,20 @@ import { als, useContext } from "./Gganbu/hook"
 import koaCompose from "koa-compose"
 import { App, createRouter } from "./Gganbu/model"
 import middleware from "./src/middleware"
-const main = async (app, port) => {
-  try {
-    app.use((ctx, next) => {
-      als.run({ ctx: ctx }, async () => {
-        // let ctx = als.getStore()
-        let ctx = useContext()
-        console.log(ctx, 123344)
-        await next()
-      }) // sets default values
-    })
-    // 注册路由
-    const Router = await createRouter()
-    app.use(koaCompose(Router))
-    app.use(koaCompose(middleware))
 
+const main = async (app, port) => {
+  const Router = await createRouter()
+  // console.log(Router)
+  try {
+    app.use(async (ctx, next) => {
+      await als.run({ ctx: ctx }, async () => {})
+      await next()
+    })
+    // 中间件注册
+    app.use(koaCompose([...middleware]))
+
+    // 注册路由
+    app.use(koaCompose(Router))
     // 启动
     const server = app.listen(port, () =>
       console.log(`项目启动, 端口：${port}, 环境：${process.env.NODE_ENV}`)
