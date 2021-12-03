85294bf418d6b4d8880f0a04602fa080beb99992
sanjayheaven <34485325+sanjayheaven@users.noreply.github.com>
2021-12-03 21:38:58 +0800

    1203|修复 port 解构失败

--- src/cli/childModule.ts
+++ src/cli/childModule.ts
@@ -7,22 +7,24 @@
 /**
  * 子进程 启动server
  */
-const createJiti = require("jiti")
-let jiti = createJiti(process.cwd(), { cache: false })
-const MODELPATH = process.env.MODELPATH // 新建子进程的时候 创建的。
-const { AppStart } = jiti(MODELPATH)
-
-;(async () => {
-  let startSuccess = false
-  try {
-    await AppStart()
-    startSuccess = true
-  } catch (e) {
-    console.log(e, 122333)
-    process.send({
-      type: "error",
-      message: "start error: " + ((e && e.message) || ""),
-    })
-  }
-  process.send({ type: "started", startSuccess })
-})()
+ const createJiti = require("jiti")
+ let jiti = createJiti(process.cwd(), { cache: false })
+ const MODELPATH = process.env.MODELPATH // 新建子进程的时候 创建的。
+ const SERVERPORT = process.env.SERVERPORT
+ const { AppStart } = jiti(MODELPATH)
+ 
+ ;(async () => {
+   let startSuccess = false
+   try {
+     await AppStart({ port: SERVERPORT })
+     startSuccess = true
+   } catch (e) {
+     console.log(e, 122333)
+     process.send({
+       type: "error",
+       message: "start error: " + ((e && e.message) || ""),
+     })
+   }
+   process.send({ type: "started", startSuccess })
+ })()
+ 
